b6fe897861b5ca1ce02b14d3df0fad46677349ca
3thanator <94484591+3thanator@users.noreply.github.com>
2021-11-16 14:44:46 -0500

    Add files via upload

---
+++ words.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 13 19:05:57 2021
+
+@author: ethan
+"""
+
+word_list = [
+    "cauchy bunyakovsky schwarz"
+    "gram schmidt",
+    "stoichiometry",
+    "pendar mahmoudi",
+    "boolean",
+    "proffesional engineers of ontario",
+    "crystallinity",
+    "base centered cubic",
+    "face centered cubic",
+    "acids and bases",
+    "laura deakin",
+    "good day for those of you who may not know me",
+    "pu chen",
+    "howard siu",
+    "mohammad kohandel",
+    "calorimetry",
+    "buffers"
+    ]
