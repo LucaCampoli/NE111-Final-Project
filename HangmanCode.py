6b56f7d1fbddf7dd1fe3a57c3d32a3d2c7c18948
3thanator <94484591+3thanator@users.noreply.github.com>
2021-12-05 17:36:43 -0500

    Update HangmanCode.py

--- HangmanCode.py
+++ HangmanCode.py
@@ -41,6 +41,7 @@ HANGMAN_PICS = ['''
  /|\  |
  / \  |
      ===''']
+#stages of hangman EC
 words = "test hello-world boolean nanotechnology waterloo goose hangman python java programming".split()
 #words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
 
@@ -48,6 +49,7 @@ def getRandomWord(wordList):
     # This function returns a random string from the passed list of strings.
     wordIndex = random.randint(0, len(wordList) - 1)
     return wordList[wordIndex]
+  #From imported wordlist.py retrieves a randomized word to be used for the game EC
 
 def displayBoard(missedLetters, correctLetters, secretWord):
     print(HANGMAN_PICS[len(missedLetters)])
@@ -59,6 +61,8 @@ def displayBoard(missedLetters, correctLetters, secretWord):
     print()
 
     blanks = '_' * len(secretWord)
+    
+    #Displays the incorrectely guessed letters EC
 
     for i in range(len(secretWord)): # Replace blanks with correctly guessed letters.
         if secretWord[i] in correctLetters:
