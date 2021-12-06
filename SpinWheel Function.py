338a8b5a4e0440c87fb4fd71c5451a51efab4c5f
RianEsguerrra <60616629+RianEsguerrra@users.noreply.github.com>
2021-12-05 22:24:26 -0500

    Update SpinWheel Function.py

--- SpinWheel Function.py
+++ SpinWheel Function.py
@@ -2,6 +2,7 @@
 
 import random
 
+#Function returns a random element from the list for the user.
 def SpinWheel():
     l=['500','550','600','650','700','750','800','850','900','5000','Bankrupt','Lose a turn','Free Play']
     return random.choice(l)
